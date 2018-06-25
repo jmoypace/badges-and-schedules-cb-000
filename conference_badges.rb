76491b727fded6138f7cd42f4d4ad7d6b58de987
jmoypace <jmoypace@gmail.com>
2018-06-25 03:28:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,7 @@ end
 def batch_badge_creator(attendees)
   storage=[]
 attendees.each do |names|
-storage.push("Hello, my name is #{names}")
+storage.push("Hello, my name is #{names}.")
 end
 storage
 end
